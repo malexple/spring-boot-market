f2701543b3591aa0531f031a011b6cfb03c1cf6b
malexple <malexple@gmail.com>
2023-11-09 22:52:30 +0300

    add product-service

---
+++ product-service/src/main/java/ru/mcs/productservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package ru.mcs.productservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.mcs.productservice.dto.ProductRequest;
+import ru.mcs.productservice.dto.ProductResponse;
+import ru.mcs.productservice.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ product-service/src/main/java/ru/mcs/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package ru.mcs.productservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import ru.mcs.productservice.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
